c1242834f97758abb947b3808d440ba1d44990ce
samflam <me@samflam.com>
2021-12-03 19:28:01 -0800

    Added commends for what each route does

--- server/routes/api.js
+++ server/routes/api.js
@@ -2,18 +2,22 @@ const express = require('express');
 
 const router = new express.Router();
 
+// Queries DB and responds with a list of objects for all messages
 router.get('/', (res, req) => {
   res.status(501).send('Not implemented')
 });
 
+// Adds new message from request to DB and respond with the new message
 router.post('/', (res, req) => {
   res.status(501).send('Not implemented')
 });
 
+// Update a message in the DB and respond with the updated message
 router.put('/:message_id', (res, req) => {
   res.status(501).send('Not implemented')
 });
 
+// Delete a message from the DB
 router.delete('/:message_id', (res, req) => {
   res.status(501).send('Not implemented')
 });
