101de5cb5ef9a5f090ab18a61b27065f0dba67bc
samflam <me@samflam.com>
2021-12-04 10:35:44 -0800

    Added session_id to messages table

--- messages.sql
+++ messages.sql
@@ -3,5 +3,6 @@ CREATE TABLE messages(
   id        SERIAL PRIMARY KEY,
   username  VARCHAR(100) NOT NULL,
   content  VARCHAR(100) NULL,
-  time_stamp date
+  time_stamp date,
+  session_id uuid
 );
