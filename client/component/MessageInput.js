58d855d293aa861f13f29acf851b067752a783ba
Albert Han <AlbertHan@macbook-pro.lan>
2021-12-04 11:31:32 -0800

    refactored send and delete buttons

--- client/component/MessageInput.js
+++ client/component/MessageInput.js
@@ -1,34 +1,22 @@
-import React, { Component } from 'react';
+import React, { Component, useState, setState } from 'react';
 
-//username
-
-//content
-/*
-
-    fetch('/api/info', {
+const MessageInput = (props) => {
+  function send() {
+    fetch('/api', {
       method: 'POST',
-      body: JSON.stringify({ character }),
+      body: JSON.stringify({ username: document.getElementById('username').value, content: document.getElementById('content').value}),
       headers: { 'Content-Type': 'application/json' },
     })
-      .then(res => res.json())
-      .then((data) => {
-        const updatedCharacter = { ...character };
-        updatedCharacter.moreInfo = {};
-        updatedCharacter.moreInfo.homeworld = data.homeworld;
-        updatedCharacter.moreInfo.films = data.films;
-        this.props.updateCharacter(character.id, updatedCharacter);
-      })
-      .catch(err => console.log('getDetails: ERROR: ', err));
-*/
-//fetch in the app?
-const MessageInput = () => {
+    .then(() => {
+      props.fetchMessages();
+      document.getElementById('content').value = '';
+    })
+  }
   return(
     <div>
-      <form action="/api" method="post">
-        <input className="textInput" name="username" placeholder="Enter username"></input>
-        <input className="textInput" name="content" placeholder="Type message here"></input>
-        <button className = "submitButton" type="submit">Send</button>
-      </form>
+        <input className="textInput" id="username" placeholder="Enter username"></input>
+        <input className="textInput" id="content" placeholder="Type message here"></input>
+        <button className = "submitButton" onClick={send}>Send</button>
     </div>
   )
 }
